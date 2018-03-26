aed7ee8561316e6c2c55b58a75ec8f9883d5f5d3
Eric Baker <ericb0774@gmail.com>
2018-03-26 10:45:59 -0500

    Initial commit. Sets up project.

---
+++ KeyPathBindings/KeyPathBindings/KeyPathBindings.h
@@ -0,0 +1,19 @@
+//
+//  KeyPathBindings.h
+//  KeyPathBindings
+//
+//  Created by Eric Baker on 26/Mar/18.
+//  Copyright © 2018 DuneParkSoftware, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyPathBindings.
+FOUNDATION_EXPORT double KeyPathBindingsVersionNumber;
+
+//! Project version string for KeyPathBindings.
+FOUNDATION_EXPORT const unsigned char KeyPathBindingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyPathBindings/PublicHeader.h>
+
+
